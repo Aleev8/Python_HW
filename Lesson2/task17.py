1cfa52bc3fc3f4e83aacc1513a0270767d6c12d0
Aleev_nb <aleev.alexandr@gmail.com>
2022-12-20 17:55:24 +0600

    Task17 has been solved

--- Lesson2/task17.py
+++ Lesson2/task17.py
@@ -2,4 +2,27 @@
 Найдите произведение элементов на указанных позициях.
 Позиции хранятся в файле file.txt в одной строке одно число. """
 
+from random import Random, randint
 
+n = int(input('Введите число элементов списка: '))
+numbers = []
+for i in range(n):
+    numbers.append(randint(-n, n + 1))
+print(numbers)
+
+f = open('file.txt', 'w')
+while True:
+    s = input('Укажите позицию, либо нажмите Enter: ')
+    if s == "":
+        break
+    f.write(s + "\n")
+f.close()
+
+result = 1
+f = open('file.txt', 'r')
+for line in f:
+    if line == "":
+        break
+    result *= numbers[int(line)]
+f.close()
+print(result)

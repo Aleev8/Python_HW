8d0f9a2f8ba02eba3cf3ee52d91e93098f0ace82
Aleev_nb <aleev.alexandr@gmail.com>
2022-12-20 18:01:01 +0600

    Task18 has been solved

--- Lesson2/task18.py
+++ Lesson2/task18.py
@@ -1,1 +1,16 @@
 """Реализуйте алгоритм перемешивания списка."""
+from random import Random, randint
+
+n = int(input('Введите длинну задаваемого списка: '))
+numbers = []
+for i in range(n):
+    numbers.append(randint(-n, n + 1))
+print(numbers)
+list = numbers[:]
+numbers_length = len(list)
+for i in range(numbers_length):
+    index = randint(0, numbers_length - 1)
+    temp = list[i]
+    list[i] = list[index]
+    list[index] = temp
+print(list)

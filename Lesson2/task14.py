a4ca8d0d63cce7ee0a53931e2a3a0a93fc4fa213
Aleev_nb <aleev.alexandr@gmail.com>
2022-12-20 16:57:06 +0600

    Task14 has been solved

--- Lesson2/task14.py
+++ Lesson2/task14.py
@@ -3,3 +3,10 @@
 Пример:
 - 6782 -> 23
 - 0,56 -> 11 """
+
+x = input('Введите число ')
+number = 0
+for i in str(x):
+    if i != '.':
+        number += int(i)
+print(f'Сумма чисел равна {number}')

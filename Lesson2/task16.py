df7be6d6505f116470a57e89ea238116c0c988a6
Zirtuff_PC <aleev.alexandr@gmail.com>
2022-12-22 15:25:26 +0600

    Task16 solved change

--- Lesson2/task16.py
+++ Lesson2/task16.py
@@ -4,11 +4,17 @@
 -Для n=4 {1: 2, 2: 2.25, 3: 2.37, 4: 2.44}
 Сумма 9.06 """
 
-n = int(input('Введите количество чисел в списке:  '))
-sum = 0
-for i in range(n):
-    a = int(input(f'Введи число {i + 1}: '))
-    a = (1+1/a)**a
-    sum += a
-    i += 1
-print('Сумма чисел равна', round(sum, 2))
+# n = int(input('Введите количество чисел в списке:  '))
+# sum = 0
+# for i in range(n):
+#     a = int(input(f'Введи число {i + 1}: '))
+#     a = (1+1/a)**a
+#     sum += a
+#     i += 1
+# print('Сумма чисел равна', round(sum, 2))
+
+n = int(input('Введите число: '))
+s = 0
+for i in range (1, n + 1):
+    s += (1 + 1 / i) ** i
+print(round(s, 2))

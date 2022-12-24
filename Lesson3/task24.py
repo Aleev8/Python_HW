d1beed1db9b407f1a5eb3f1dacb277ff556b19a1
Aleev_nb <aleev.alexandr@gmail.com>
2022-12-24 09:59:41 +0600

    Task24 has been solved

--- Lesson3/task24.py
+++ Lesson3/task24.py
@@ -4,3 +4,10 @@
 Пример:
 - [1.1, 1.2, 3.1, 5, 10.01] => 0.19"""
 
+import random
+list_len = random.randint(5, 12)
+test_list = []
+for i in range(list_len):
+    test_list.append(round(random.uniform(0, 9), 2))
+result_list = [round(i%1,2) for i in test_list]
+print(test_list, '=>', round(max(result_list) - min(result_list), 2))

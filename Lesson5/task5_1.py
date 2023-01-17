4a584b83d218675aef908d934fb2536189fafb4d
Aleev_nb <aleev.alexandr@gmail.com>
2023-01-17 11:27:44 +0600

    Task5_1 has been solved

---
+++ Lesson5/task5_1.py
@@ -0,0 +1,11 @@
+#Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
+
+my_text = 'Напишите абв напиабв програбвмму программу, удаляющую из \
+    этого абв текста все вабвс слова, содерабващие содержащие "абв"'
+
+def del_some_words(my_text):
+    my_text = list(filter(lambda word: 'абв' not in word, my_text.split()))
+    return " ".join(my_text)
+
+my_text = del_some_words(my_text)
+print(my_text)

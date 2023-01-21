aece7096cb2b3a2a4b8700754a541bc80969bb54
Aleev_nb <aleev.alexandr@gmail.com>
2023-01-21 13:30:46 +0600

    Task6_1 has been solved

---
+++ Lesson6/task6_1.py
@@ -0,0 +1,12 @@
+#Создайте список из случайных чисел. Найдите количество различных
+# элементов в нем.
+
+import random
+list1 = [random.randint(1, 15) for i in range(random.randint(10, 18))]
+print("Сгенерирован список: ", list1)
+list_res = []
+for i in range(len(list1)):
+    if list1.count(list1[i]) == 1:
+        list_res.append(list1[i])
+print('Количество различающихся элементов=', len(list_res))
+

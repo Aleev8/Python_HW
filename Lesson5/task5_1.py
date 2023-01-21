2333e8f4fac42a8bac65113e932d9563ae0bd6bf
Aleev_nb <aleev.alexandr@gmail.com>
2023-01-21 09:58:33 +0600

    Added solved teacher task5_1

--- Lesson5/task5_1.py
+++ Lesson5/task5_1.py
@@ -1,11 +1,16 @@
 #Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
 
-my_text = 'Напишите абв напиабв програбвмму программу, удаляющую из \
-    этого абв текста все вабвс слова, содерабващие содержащие "абв"'
+# my_text = 'Напишите абв напиабв програбвмму программу, удаляющую из \
+#     этого абв текста все вабвс слова, содерабващие содержащие "абв"'
+#
+# def del_some_words(my_text):
+#     my_text = list(filter(lambda word: 'абв' not in word, my_text.split()))
+#     return " ".join(my_text)
+#
+# my_text = del_some_words(my_text)
+# print(my_text)
 
-def del_some_words(my_text):
-    my_text = list(filter(lambda word: 'абв' not in word, my_text.split()))
-    return " ".join(my_text)
-
-my_text = del_some_words(my_text)
-print(my_text)
+## Решение преподавателя
+text = input().split()
+res_text = list(filter(lambda word: 'а' not in word and 'б' not in word and 'в' not in word, text))
+print(res_text)

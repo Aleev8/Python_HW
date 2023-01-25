f32c453e64a105b3a72a91fb5e810ebe9b914a95
Zirtuff_PC <aleev.alexandr@gmail.com>
2023-01-25 13:52:24 +0600

    Added solved teacher task6_1

--- Lesson6/task6_1.py
+++ Lesson6/task6_1.py
@@ -10,3 +10,8 @@ for i in range(len(list1)):
         list_res.append(list1[i])
 print('Количество различающихся элементов=', len(list_res))
 
+#Решение преподавателя
+some_list = [random.randint(1, 15) for i in range(random.randint(10, 18))]
+print("Сгенерирован список: ", some_list)
+some_set = set(some_list)
+print(len(some_set))

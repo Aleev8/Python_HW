246729a8701a3dbc0514b14a43343c0c35d80208
Zirtuff_PC <aleev.alexandr@gmail.com>
2023-02-05 12:57:22 +0600

    Create t_bot

---
+++ Lesson10_t_bot/bot.py
@@ -0,0 +1,77 @@
+import requests
+import datetime
+import logging
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+# 'UTF-8-sig'
+logging.basicConfig(level=logging.INFO, filename="bot_log.csv", filemode="w",
+                    format="%(asctime)s: %(levelname)s %(funcName)s-%(lineno)d %(message)s")
+
+open_weather_token = "4a460d4cf89323def19702d6e144d116"
+tg_bot_token = "5892118533:AAECPs68dddAcRFyAAcYrAr6NFFc6Ho-wUI"
+
+bot = Bot(token=tg_bot_token)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=["start"])
+async def start_command(message: types.Message):
+    user_id = message.from_user.id
+    user_name = message.from_user.first_name
+    user_full_name = message.from_user.full_name
+    user_bot = message.from_user.is_bot
+    user_message = message.text
+    logging.info(f'{user_id=} {user_bot=} {user_message=}')
+    await message.reply(f"Привет, {user_full_name} Напиши мне название города и я пришлю сводку погоды!")
+
+
+@dp.message_handler()
+async def get_weather(message: types.Message):
+    code_to_smile = {
+        "Clear": "Ясно \U00002600",
+        "Clouds": "Облачно \U00002601",
+        "Rain": "Дождь \U00002614",
+        "Drizzle": "Дождь \U00002614",
+        "Thunderstorm": "Гроза \U000026A1",
+        "Snow": "Снег \U0001F328",
+        "Mist": "Туман \U0001F32B"
+    }
+
+    try:
+        r = requests.get(
+            f"http://api.openweathermap.org/data/2.5/weather?q={message.text}&appid={open_weather_token}&units=metric"
+        )
+        data = r.json()
+
+        city = data["name"]
+        cur_weather = data["main"]["temp"]
+
+        weather_description = data["weather"][0]["main"]
+        if weather_description in code_to_smile:
+            wd = code_to_smile[weather_description]
+        else:
+            wd = "Посмотри в окно, не пойму что там за погода!"
+
+        humidity = data["main"]["humidity"]
+        pressure = data["main"]["pressure"]
+        wind = data["wind"]["speed"]
+        sunrise_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
+        sunset_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunset"])
+        length_of_the_day = datetime.datetime.fromtimestamp(data["sys"]["sunset"]) - datetime.datetime.fromtimestamp(
+            data["sys"]["sunrise"])
+
+        await message.reply(f"***{datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}***\n"
+              f"Погода в городе: {city}\nТемпература: {cur_weather}C° {wd}\n"
+              f"Влажность: {humidity}%\nДавление: {pressure} мм.рт.ст\nВетер: {wind} м/с\n"
+              f"Восход солнца: {sunrise_timestamp}\nЗакат солнца: {sunset_timestamp}\nПродолжительность дня: {length_of_the_day}\n"
+              f"***Хорошего дня!***"
+              )
+
+    except:
+        await message.reply("\U00002620 Проверьте название города \U00002620")
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)

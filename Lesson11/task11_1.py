5a3768fd1ecae302ec75d333a350cf307d5a4ba4
Zirtuff_PC <aleev.alexandr@gmail.com>
2023-02-07 20:56:12 +0600

    Task11_1 change solved

--- Lesson11/task11_1.py
+++ Lesson11/task11_1.py
@@ -8,70 +8,24 @@
 # 6.Определить промежутки, на котором f > 0
 # 7.Определить промежутки, на котором f < 0
 
+import sympy
+import matplotlib.pyplot as plt
+import numpy as np
+from sympy import symbols
+from sympy import solveset
+from sympy.abc import x
+from sympy import sin, cos, pi
+import pylab
 
-# Дана функция f(x) = -8x^2+3x+17
-# 1 Опоределить корни
 
-# from sympy import *
-#
-# x = Symbol('x')
-# func=-8*x**2+3*x+17
-# y=solve(func)
-# x1=float(y[0])
-# x2=float(y[1])
-# print(x1,x2)
-#
-# # 2 Найти интервалы, на которых функция возрастает
-#
-# fd=diff(func)
-# print(solve(0<fd))
-#
-# # 3 Найти интервалы, на которых функция убывает
-#
-# print(solve(fd<0))
-#
-# # 4 Построить график
-#
-# import matplotlib.pyplot as plt
-# list_y=[]
-# for i in range(-5,6):
-#     x=i
-#     y=-8*x**2+3*x+17
-#     list_y.append(y)
-# print(list_y)
-# plt.plot(range(-5,6),[0,0,0,0,0,0,0,0,0,0,0])
-# plt.plot(range(-5,6),list_y)
-#
-# # 5 Вычислить вершину
-#
-# corni=solve(fd)
-# top=corni[0]
-# x=top
-# y=-8*x**2+3*x+17
-# print(top,y)
-#
-# # 6 Определить промежутки, на котором f > 0
-#
-# solve(0<func)
-#
-# # 7 Определить промежутки, на котором f < 0
-#
-# solve(func<0)
+x = sympy.symbols('x')
+f = -12*(x**4)*sin(cos(x)) - 18*x**3 + 5*x**2 + 10*x - 30
+roots = solveset(f)
+print(roots)
 
-from sympy import *
-x = Symbol('x')
-y = -12*x**4
-solve(y)
+x = np.arange(-5, 5, 0.01)
 
-x = Symbol('x')
-y = sin(cos(x))
-solve(y)
+f = f = -12*(x**4)*np.sin(np.cos(x)) - 18*x**3 + 5*x**2 + 10*x - 30
 
-x = x = Symbol('x')
-y = -18*x**3+5*x**2 + 10*x-30
-solve(y)
-
-x = Symbol('x')
-y = -12*x**4*sin(cos(x)) - 18*x**3+5*x**2 + 10*x-30
-plot(y)
-print(y)
+plt.plot(x, f)
+plt.grid(color='g')
